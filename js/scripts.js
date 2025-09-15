b649a7757c5e17430ef9a35be02559b13d066243
muthu kumar <muthu2085kumar@gmail.com>
2025-09-15 16:48:44 +0530

    msg update

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,34 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Array to store form data objects
+const formDataArray = [];
+
+// Get form and display div
+const form = document.getElementById('subscribeForm');
+const formDataList = document.getElementById('formDataList');
+
+if (form) {
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		// Get form values
+		const name = document.getElementById('name').value;
+		const email = document.getElementById('email').value;
+		// Store as object
+		const formData = { name, email };
+		formDataArray.push(formData);
+		// Display all form data using map
+		displayFormData();
+		// Optionally reset form
+		form.reset();
+	});
+}
+
+function displayFormData() {
+	if (!formDataList) return;
+	formDataList.innerHTML = '<h5>Submitted Data:</h5>' +
+		formDataArray.map((data, idx) =>
+			`<div>${idx + 1}. Name: <b>${data.name}</b>, Email: <b>${data.email}</b></div>`
+		).join('');
+}
